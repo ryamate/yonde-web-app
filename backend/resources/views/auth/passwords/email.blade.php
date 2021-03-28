b90a51e9f1b8055c55a39bb71bc8f103f0f693bf
ryamate <iiiiiii.satenanbon@gmail.com>
2021-03-28 15:39:27 +0900

    [feat] Sendgridを使ったパスワード再設定機能 closed #22

--- backend/resources/views/auth/passwords/email.blade.php
+++ backend/resources/views/auth/passwords/email.blade.php
@@ -3,13 +3,8 @@
 @section('title', 'パスワード再設定-Yonde-')
 
 @section('content')
-<nav class="navbar navbar-light bg-teal1 shadow-sm bd-navbar justify-content-center"
-    style="vertical-align: middle; position: sticky; top: 0; z-index: 1071; background: linear-gradient(-135deg, #22968a, #45d9c8) fixed; opacity: 0.97;">
-    <a class="navbar-brand p-0" href="/">
-        <img src="{{ asset('image/logo.png') }}" height="45"
-            class="d-inline-block align-center text-white text-decoration-none" alt="yonde">
-    </a>
-</nav>
+
+@include('auth.nav')
 
 <div class="bg-light">
     <div class="container">
---
+++ backend/resources/views/auth/passwords/reset.blade.php
@@ -0,0 +1,52 @@
+@extends('app')
+
+@section('title', 'パスワード再設定-Yonde-')
+
+@section('content')
+
+@include('auth.nav')
+
+<div class="bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6 mt-4" style="margin-bottom: 90px">
+                <h2 class="text-center"><a class="text-dark text-decoration-none">パスワードの再設定</a></h2>
+                <p class="pt-4" style="font-size: 14px;">
+                    　新しいログインパスワードを設定します。
+                </p>
+                <div class="card mt-4 p-4 shadow-sm">
+
+                    @include('error_card_list')
+
+                    <form method="POST" action="{{ route('password.update') }}">
+                        @csrf
+
+                        <input type="hidden" name="email" value="{{ $email }}">
+                        <input type="hidden" name="token" value="{{ $token }}">
+
+                        <div class="form-group">
+                            <label for="password">新しいパスワード</label>
+                            <input class="form-control" type="password" id="password" name="password" required
+                                placeholder="パスワードを作成">
+                            <ul class="text-dark small">
+                                <li>半角英数・記号：8文字以上</li>
+                            </ul>
+                        </div>
+                        <div class="form-group">
+                            <label for="password_confirmation">新しいパスワード(確認)</label>
+                            <input class="form-control" type="password" id="password_confirmation"
+                                name="password_confirmation" required placeholder="パスワードを確認">
+                        </div>
+
+                        <button type="submit"
+                            class="btn btn-block bg-white btn-outline-teal1 text-decoration-none text-teal1 mt-4"><b>再設定する</b></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @include('footer')
+
+</div>
+@endsection

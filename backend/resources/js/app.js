de11420090e7fc8c49942a28f526989f7cae813d
ryamate <iiiiiii.satenanbon@gmail.com>
2021-05-10 15:43:03 +0900

    タグ入力のVueコンポーネント表示

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -1,10 +1,12 @@
 import './bootstrap'
 import Vue from 'vue'
 import ReviewLike from './components/ReviewLike'
+import ReviewTagsInput from './components/ReviewTagsInput'
 
 const app = new Vue({
     el: '#app',
     components: {
         ReviewLike,
+        ReviewTagsInput,
     }
 })

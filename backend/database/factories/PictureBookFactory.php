a0096650dde93655b732a76e634a1fe3bb5676c2
ryamate <iiiiiii.satenanbon@gmail.com>
2021-05-19 07:25:49 +0900

    いいねされているかを判定するメソッドのテスト

---
+++ backend/database/factories/PictureBookFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\PictureBook;
+use App\User;
+use Faker\Generator as Faker;
+
+$factory->define(PictureBook::class, function (Faker $faker) {
+    return [
+        'google_books_id' => $faker->text(50),
+        'isbn_13' => $faker->text(13),
+        'title' => $faker->text(50),
+        'authors' => $faker->text(50),
+        'published_date' => $faker->text(50),
+        'thumbnail_uri' => $faker->url,
+        'five_star_rating' => $faker->randomDigit() . "\n",
+        'read_status' => $faker->randomDigit() . "\n",
+        'review' => $faker->text(500),
+        'user_id' => function () {
+            return factory(User::class);
+        }
+    ];
+});
---
+++ backend/tests/Feature/PictureBookTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\PictureBook;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PictureBookTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testIsLikedByNull()
+    {
+        $pictureBook = factory(PictureBook::class)->create();
+
+        $result = $pictureBook->isLikedBy(null);
+
+        $this->assertFalse($result);
+    }
+
+    public function testIsLikedByTheUser()
+    {
+        $pictureBook = factory(PictureBook::class)->create();
+        $user = factory(User::class)->create();
+        $pictureBook->likes()->attach($user);
+
+        $result = $pictureBook->isLikedBy($user);
+
+        $this->assertTrue($result);
+    }
+
+    public function testIsLikedByAnother()
+    {
+        $pictureBook = factory(PictureBook::class)->create();
+        $user = factory(User::class)->create();
+        $another = factory(User::class)->create();
+        $pictureBook->likes()->attach($another);
+
+        $result = $pictureBook->isLikedBy($user);
+
+        $this->assertFalse($result);
+    }
+}
